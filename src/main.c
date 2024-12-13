f58b12b0d521c6f3c556c4080f5672a0a1b0ac14
Katja den Dulk <katja.dendulk@vstep.lan>
2024-12-13 13:24:55 +0100

    Added OpenGL boilerplate

--- src/main.c
+++ src/main.c
@@ -10,6 +10,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  GLFWwindow *window = glfwCreateWindow(1024, 768, "Ella", NULL, NULL);
+  if (!window) {
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    return 1;
+  }
+
+  glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+  }
+
   fprintf(stdout, "%s\n", argv[0]);
   return 0;
 }
