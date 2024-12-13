7189fb0dcc0cead7515657b5d2817fd15799c84a
Katja den Dulk <katja.dendulk@vstep.lan>
2024-12-13 13:19:39 +0100

    Added submodules

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
 int main(int argc, char **argv) {
+  if (!glfwInit()) {
+    return 1;
+  }
+
   fprintf(stdout, "%s\n", argv[0]);
   return 0;
 }
